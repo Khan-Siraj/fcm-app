b940f23662c36138086c5f16a7b1263e203e82ad
Dev Siraj Khan <sirajk78620@gmail.com>
2024-05-10 11:48:26 +0530

    custom notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,6 +39,24 @@ export class AppComponent implements OnInit {
     onMessage(messaging, (payload) => {
       console.log('Message received. ', payload);
       this.message=payload;
+      this.showCustomNotification(payload)
     });
+  }
+
+  showCustomNotification (payload: any) {
+    let notify_data = payload ['notification'];
+    let title = notify_data['title'];
+    let options = {
+    body: notify_data['body' ],
+    icon: "./assets/healthmagus.png",
+    badge: "./assets/healthmagus.png",
+    image: "./assets/healthmagus.png",
+    };
+    console.log("new message received. ", notify_data);
+    let notify: Notification = new Notification (title, options)
+    notify.onclick= event => {
+    event.preventDefault();
+    window.location.href='https://www.google.com';
+    }
   }
 }
